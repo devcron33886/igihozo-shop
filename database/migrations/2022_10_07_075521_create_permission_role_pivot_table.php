0744bc9b0d9bcd09a0d77a61dc5b731c32a56d98
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-10-07 15:11:38 +0200

    fixed permission role

--- database/migrations/2022_10_07_075521_create_permission_role_pivot_table.php
+++ database/migrations/2022_10_07_075521_create_permission_role_pivot_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->foreignId('role_id')->references('id')->on('roles')->cascadeOnDelete();
-            $table->foreignId('permission_id')->references('id')->cascadeOnDelete();
+            $table->foreignId('permission_id')->references('id')->on('permisiions')->cascadeOnDelete();
         });
     }
 
