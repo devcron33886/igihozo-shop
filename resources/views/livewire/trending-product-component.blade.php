3aec095f50a7f94c494fc843ee267f5108b6f67b
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2022-10-23 11:05:36 +0200

    Updated cart with listener

--- resources/views/livewire/trending-product-component.blade.php
+++ resources/views/livewire/trending-product-component.blade.php
@@ -1,26 +1,25 @@
 <div class="col-lg-4 col-md-6 col-12">
     <div class="single-product">
         <div class="product-image">
-            <img src="{{ $product->getFirstMediaUrl('image', 'preview') }}"
-                 alt="{{ $product->name }}">
-            @if ($added)
-                <button type="button" wire:click="remove" wire:loading.attr="disabled" class="btn btn-danger">
-                    <i class="lni lni-close"></i>
-                    Remove
-                </button>
-
-            @else
-                <button type="button" wire:loading.attr="disabled" wire:click="addToCart" class="btn btn-primary">
-                    <i class="lni lni-cart"></i>&nbsp;
-                    Add to Cart
-                </button>
-            @endif
+            <img src="{{ $product->getFirstMediaUrl('image', 'preview') }}" alt="{{ $product->name }}" style="height:400px !important;">
+            <div class="py-4">
+                @if ($added)
+                    <button type="button" wire:click="remove" wire:loading.attr="disabled" class="btn btn-danger">
+                        <i class="lni lni-close"></i>
+                        Remove
+                    </button>
+                @else
+                    <button type="button" wire:loading.attr="disabled" wire:click="addToCart" class="btn btn-primary">
+                        <i class="lni lni-cart"></i>&nbsp;
+                        Add to Cart
+                    </button>
+                @endif
+            </div>
         </div>
         <div class="product-info">
             <span class="category">{{ $product->category->name }}</span>
             <h4 class="title">
-                <a
-                    href="{{ route('product-details', $product->slug) }}">{{ $product->name }}</a>
+                <a href="{{ route('product-details', $product->slug) }}">{{ $product->name }}</a>
             </h4>
             <ul class="review">
             </ul>
@@ -30,4 +29,3 @@
         </div>
     </div>
 </div>
-
